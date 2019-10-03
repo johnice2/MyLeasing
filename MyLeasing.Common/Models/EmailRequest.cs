36de0da02718033007aa84d57731541f5bce4ec3
John <johnice2@gmail.com>
2019-10-03 11:13:50 -0500

    Api OwnersController , CommonModels

---
+++ MyLeasing.Common/Models/EmailRequest.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasing.Common.Models
+{
+    public class EmailRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ MyLeasing.Web/Controllers/ContractsController.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyLeasing.Web.Data;
+
+namespace MyLeasing.Web.Controllers
+{
+    [Authorize(Roles = "Manager")]
+    public class ContractsController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public ContractsController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_dataContext.Contracts
+                .Include(c => c.Owner)
+                .ThenInclude(o => o.User)
+                .Include(c => c.Lessee)
+                .ThenInclude(l => l.User)
+                .Include(c => c.Property)
+                .ThenInclude(p => p.PropertyType));
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contract = await _dataContext.Contracts
+                .Include(c => c.Owner)
+                .ThenInclude(o => o.User)
+                .Include(c => c.Lessee)
+                .ThenInclude(l => l.User)
+                .Include(c => c.Property)
+                .ThenInclude(p => p.PropertyType)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+
+            return View(contract);
+        }
+    }
+}
