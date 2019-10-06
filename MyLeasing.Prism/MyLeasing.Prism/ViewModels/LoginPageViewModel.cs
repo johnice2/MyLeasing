32b41b4d7ab71e55e46b5d26696fc8e7c339b76c
John <johnice2@gmail.com>
2019-10-06 16:41:19 -0500

    check internet connection

--- MyLeasing.Prism/MyLeasing.Prism/ViewModels/LoginPageViewModel.cs
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/LoginPageViewModel.cs
@@ -70,13 +70,24 @@ namespace MyLeasing.Prism.ViewModels
             IsRunning = true;
             IsEnabled = false;
 
+            var url = App.Current.Resources["UrlAPI"].ToString();
+            var connection = await _apiService.CheckConnectionAsync(url);
+            if (!connection)
+            {
+                IsEnabled = true;
+                IsRunning = false;
+                await App.Current.MainPage.DisplayAlert("Error", "Check the internet connection.", "Accept");
+                return;
+            }
+
+
             var request = new TokenRequest
             {
                 Password = Password,
                 Username = Email
             };
 
-            var url = App.Current.Resources["UrlAPI"].ToString();
+        
             var response = await _apiService.GetTokenAsync(url, "Account", "/CreateToken", request);
 
             IsRunning = false;
