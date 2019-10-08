3a44267c3fd130a1bfbe2dff52156f13455000a2
John <johnice2@gmail.com>
2019-10-08 11:38:34 -0500

    Navigate to property

--- MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertiesPageViewModel.cs
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertiesPageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using MyLeasing.Common.Models;
+using MyLeasing.Prism.ViewsModels;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
@@ -11,18 +12,19 @@ namespace MyLeasing.Prism.ViewModels
 {
     public class PropertiesPageViewModel : ViewModelBase
     {
-
+        private readonly INavigationService _navigationService;
         private OwnerResponse _owner;
-        private ObservableCollection<PropertyResponse> _properties;
+        private ObservableCollection<PropertyItemViewModel> _properties;
         
 
         public PropertiesPageViewModel(
             INavigationService navigationService) : base(navigationService)
         {
             Title = "Properties";
+            _navigationService = navigationService;
         }
 
-        public ObservableCollection<PropertyResponse> Properties
+        public ObservableCollection<PropertyItemViewModel> Properties
         {
             get => _properties;
             set => SetProperty(ref _properties, value);
@@ -36,7 +38,23 @@ namespace MyLeasing.Prism.ViewModels
             {
                 _owner = parameters.GetValue<OwnerResponse>("owner");
                 Title = $"Properties of: {_owner.FullName}";
-                Properties = new ObservableCollection<PropertyResponse>(_owner.Properties);
+                Properties = new ObservableCollection<PropertyItemViewModel>(_owner.Properties.Select(p => new PropertyItemViewModel(_navigationService)
+                {
+                    Address = p.Address,
+                    Contracts = p.Contracts,
+                    HasParkingLot = p.HasParkingLot,
+                    Id = p.Id,
+                    IsAvailable = p.IsAvailable,
+                    Neighborhood = p.Neighborhood,
+                    Price = p.Price,
+                    PropertyImages = p.PropertyImages,
+                    PropertyType = p.PropertyType,
+                    Remarks = p.Remarks,
+                    Rooms = p.Rooms,
+                    SquareMeters = p.SquareMeters,
+                    Stratum = p.Stratum
+
+                }).ToList());
             }
         }
     }
---
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertyItemViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MyLeasing.Common.Models;
+using Prism.Commands;
+using Prism.Navigation;
+
+namespace MyLeasing.Prism.ViewsModels
+{
+    public class PropertyItemViewModel : PropertyResponse
+    {
+        private readonly INavigationService _navigationService;
+        private DelegateCommand _selectPropertyCommand;
+
+        public PropertyItemViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public DelegateCommand SelectPropertyCommand => _selectPropertyCommand ?? (_selectPropertyCommand = new DelegateCommand(SelectProperty));
+
+        private async void SelectProperty()
+        {
+            var parameters = new NavigationParameters
+            {
+                { "property", this }
+            };
+            await _navigationService.NavigateAsync("PropertyPage", parameters);
+        }
+    }
+}
